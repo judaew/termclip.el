caaf0247bffd88a680d9b8639bfb05c120038295
Vadym-Valdis Yudaiev <judaew@outlook.de>
2025-04-12 01:37:27 +0300

    Add support X.Org & macOS, better parsing cmd args

--- termclip.el
+++ termclip.el
@@ -2,7 +2,7 @@
 
 ;; Author: Vadym-Valdis Yudaiev
 ;; Version 0.1
-;; Keywords: clipboard, terminal, Wayland
+;; Keywords: clipboard, terminal, Wayland, X.Org, macOS
 ;; Homepage: https://github.com/judaew/termclip.el
 ;; SPDX-License-Identifier: MIT
 
@@ -13,38 +13,96 @@
 
 ;;; Code:
 
-(defun termclip/kill-region (begin end)
+(defgroup termclip nil
+  "Terminal Clipboard Integration."
+  :group 'external
+  :prefix "termclip-"
+  :link '(url-link "https://github.com/judaew/termclip.el"))
+
+(defcustom termclip-clipboard-tool 'wayland
+  "The executable tool to be used with termclip for copying/pasting.
+
+Available options:
+- \=wayland\= (for Wayland) [default]
+- \=xorg-xsel\= (for X.org)
+- \=xorg-xclip\= (for X.org)
+- \=macos\= (for macOS)"
+  :type '(choice
+	  (const :tag "wl-clipboard (for Wayland)" wayland)
+	  (const :tag "xsel (for X.org)" xorg-xsel)
+	  (const :tag "xclip (for X.org)" xorg-xclip)
+	  (const :tag "pbcopy/pbpaste (for macOS)" macos))
+  :group 'termclip)
+
+(defun termclip--get-command (type)
+  "Return the copy/paste (TYPE) command based on `termclip-clipboard-tool'.
+TYPE must be either \=copy\= or \=paste\="
+  (let ((cmd (pcase (cons termclip-clipboard-tool type)
+	       ('(wayland . copy ) "wl-copy")
+	       ('(xorg-xsel . copy) "xsel --clipboard -i")
+	       ('(xorg-xclip . copy) "xclip -selection clipboard")
+	       ('(macos . copy) "pbcopy")
+	       ('(wayland . paste) "wl-paste --no-newline")
+	       ('(xorg-xsel . paste) "xsel --clipboard -o")
+	       ('(xorg-xclip . paste) "xclip -selection clipboard -o")
+	       ('(macos . paste) "pbpaste")
+	       (_ (error "Unsupported clipboard tool: '%s'-'%s'"
+			 termclip-clipboard-tool type)))))
+    (let ((bin (car (split-string cmd))))
+      (unless (executable-find bin)
+	(error "Clipboard tool '%s' not found in PATH" bin)))
+    cmd))
+
+(defun termclip-kill-region (begin end)
   "Cut region between BEGIN and END to system clipboard."
   (interactive "r")
-  (call-process-region begin end "wl-copy")
-  (delete-region begin end))
+  (condition-case err
+      (let* ((raw-cmd (termclip--get-command 'copy))
+             (cmd-parts (split-string-and-unquote raw-cmd)))
+        
+        (apply #'call-process-region begin end
+               (car cmd-parts) nil nil nil
+               (cdr cmd-parts))
+        
+        (delete-region begin end))
+    
+    (error
+     (message "Failed to cut to clipboard: %s" (error-message-string err)))))
 
-(defun termclip/kill-ring-save (begin end)
+(defun termclip-kill-ring-save (begin end)
   "Copy region between BEGIN and END to system clipboard."
   (interactive "r")
-  (call-process-region begin end "wl-copy")
-  (deactivate-mark))
+  (condition-case err
+      (let* ((raw-cmd (termclip--get-command 'copy))
+             (cmd-parts (split-string-and-unquote raw-cmd)))
+        (apply #'call-process-region begin end
+               (car cmd-parts) nil nil nil
+               (cdr cmd-parts))
+	
+	(deactivate-mark))
+    (error
+     (message "Failed to copy to clipboard: %s" (error-message-string err)))))
 
-(defun termclip/yank ()
+(defun termclip-yank ()
   "Paste contents from system clipboard."
   (interactive)
-  (let ((clipboard-text (shell-command-to-string "wl-paste --no-newline")))
-    (insert clipboard-text)))
+  (condition-case err
+      (let* ((raw-cmd (termclip--get-command 'paste))
+             (cmd-parts (split-string-and-unquote raw-cmd))
+             (clipboard-text (shell-command-to-string (string-join cmd-parts " "))))
+        (insert clipboard-text))
+    (error
+     (message "Failed to paste from clipboard: %s"
+	      (error-message-string err)))))
 
 (defvar termclip-mode-map (make-sparse-keymap)
   "Keymap for terminal clipboard mode.")
 
-(defun termclip/enable-bindings ()
+(defun termclip--enable-bindings ()
   "Enable terminal clipboard keybindings."
-  (define-key termclip-mode-map (kbd "C-w") #'termclip/kill-region)
-  (define-key termclip-mode-map (kbd "M-w") #'termclip/kill-ring-save)
-  (define-key termclip-mode-map (kbd "C-y") #'termclip/yank))
-
-(defun termclip/disable-bindings ()
-  "Disable terminal clipboard keybindings."
-  (define-key termclip-mode-map (kbd "C-w") #'kill-region)
-  (define-key termclip-mode-map (kbd "M-w") #'kill-ring-save)
-  (define-key termclip-mode-map (kbd "C-y") #'yank))
+  (define-key termclip-mode-map [remap kill-region] #'termclip-kill-region)
+  (define-key termclip-mode-map [remap kill-ring-save] #'termclip-kill-ring-save)
+  (define-key termclip-mode-map [remap yank] #'termclip-yank))
 
 ;;;###autoload
 (define-minor-mode termclip-mode
@@ -54,21 +112,18 @@
   :lighter "termclip"
   :keymap termclip-mode-map
   (if termclip-mode
-      (termclip/enable-bindings)
-    (termclip/disable-bindings)))
+      (termclip--enable-bindings)))
 
 ;;;###autoload
-(defun termclip/setup-if-needed (frame)
+(defun termclip-setup-if-needed (frame)
   "Configure termclip-mode for newly created FRAME."
   (with-selected-frame frame
-    (if (not (display-graphic-p))
-	(termclip-mode 1)
-      (termclip-mode 0))))
+    (termclip-mode (if (display-graphic-p) 0 1))))
 
 (if (daemonp)
-    (add-hook 'after-make-frame-functions #'termclip/setup-if-needed)
+    (add-hook 'after-make-frame-functions #'termclip-setup-if-needed)
   (unless (display-graphic-p)
-    termclip-mode 1))
+    (termclip-mode 1)))
 
 (provide 'termclip)
 
